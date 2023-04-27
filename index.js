85c479ffde4e92b033bdd18a89bef95154451b7f
David Servias <davidservias@gmail.com>
2023-04-27 08:20:17 -0700

    update origin

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ connect(process.env.REACT_APP_DB_URI, {useNewUrlParser: true, useUnifiedTopology
 
 // Apply CORS policy
 app.use(cors({
-    origin: process.env.ORIGIN
+    //origin: process.env.ORIGIN
+    origin: "https://bucketlist-omft.onrender.com/"
 }));
 
 
